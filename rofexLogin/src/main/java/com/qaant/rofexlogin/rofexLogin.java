f7b8a21a6a101128ae6e20cd1313a6ba7e9b0732
Pauli <pjseoane@gmail.com>
2019-05-11 11:15:29 -0300

    rofexLogin

--- rofexLogin/src/main/java/com/qaant/rofexlogin/rofexLogin.java
+++ rofexLogin/src/main/java/com/qaant/rofexlogin/rofexLogin.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 
  */
 package com.qaant.rofexlogin;
 
@@ -11,7 +9,7 @@ import java.util.Base64;
 
 /**
  *
- * @author pauli
+ * @author pauli ver 2.0
  */
 public class rofexLogin {
     String url, usr, pswd;
